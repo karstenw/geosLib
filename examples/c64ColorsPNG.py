614c0d391e948be879bfa409ff09d8dd512ed15b
Karsten Wolf <karstenwo@web.de>
2016-06-07 14:43:02 +0200

    Better variable names.

--- examples/c64ColorsPNG.py
+++ examples/c64ColorsPNG.py
@@ -20,6 +20,7 @@ kwlog = 0
 
 import geosLib
 
+imagesize = 256
 
 if __name__ == '__main__':
     # make an image with the c64 colors
@@ -27,16 +28,15 @@ if __name__ == '__main__':
     import PIL.Image
     import PIL.ImageDraw
 
-    c = 256
-    d = c >> 2
-    c64colrect = PIL.Image.new('RGB', (c,c), (1,1,1))
+    tilewidth = imagesize >> 2
+    c64colrect = PIL.Image.new('RGB', (imagesize,imagesize), (1,1,1))
     draw = PIL.ImageDraw.Draw( c64colrect )
     for y in range(4):
         for x in range(4):
             col = y * 4 + x
-            xc = x * d
-            yc = y * d
+            xc = x * tilewidth
+            yc = y * tilewidth
             col = geosLib.c64colors[col]
-            draw.rectangle((xc,yc,xc+d,yc+d), fill=col, outline=None)
+            draw.rectangle((xc,yc,xc+tilewidth,yc+tilewidth), fill=col, outline=None)
         
     c64colrect.save("C64 colors.png")
